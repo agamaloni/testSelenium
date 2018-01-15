d323f25848b5b31af3c40f4fad338d941e855719
agam aloni <agamaloni@gmail.com>
2018-01-15 06:32:33 -0800

    Update PagesPath.java

--- src/main/java/selenium/com/PagesPath.java
+++ src/main/java/selenium/com/PagesPath.java
@@ -6,7 +6,7 @@ import com.gargoylesoftware.htmlunit.Page;
  * Created by aloni on 27/05/17.
  */
 public final class PagesPath {
-    public static final String HOME_PAGE = "https://www.skyboxsecurity.com";
-    public static final String CONTACT_PAGE ="https://lp.skyboxsecurity.com/WEB-Contact-Us.html";
+    public static final String HOME_PAGE = "";
+    public static final String CONTACT_PAGE ="";
     public static final String GOOGLE = "http://www.google.co.il";
 }
